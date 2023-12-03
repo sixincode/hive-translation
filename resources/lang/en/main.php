8a8178e4f9a5ef825b977cb347190a2daaa3343f
aalexkarim <contact.alexkarim@gmail.com>
2023-12-02 22:18:25 -0500

    auto-update-1701573503

--- resources/lang/en/main.php
+++ resources/lang/en/main.php
@@ -1,13 +1,26 @@
 <?php
 
 return [
+  "allRightsR"      => "All rights reserved.",
   "about"           => "About",
+  "aboutUs"         => "About Us",
   "blog"            => "Blog",
   "by6ixin"         => "by 6ixin",
   "cmgsoon"         => "Coming Soon",
   "contact"         => "Contact",
+  "contactU"        => "Contact Us",
+  'email'           => 'Email',
+  'fromTo'          => 'from :from to :to',
+  "getInT"          => "Get in touch",
   "home"            => "Home",
+  "mon-fri"         => "Mon-Fri",
   "joinUS"          => "Join Us",
   "joinUsTd"        => "Join Us today",
-  "sgupnewslt"      => "Sign Up to our newsletter",
+  "ourSupTeamChat"  => "Our support team is always here to chat.",
+  "ourSupTeamHelp"  => "Our support team is always here to help.",
+  "phone"           => "Phone",
+  "privP"           => "privacy&nbsp;policy",
+  "publicx"         => "Publications",
+  "weAreHTohelp"    => "We’re here to help. Please contact us and we’ll make sure you get the information you need.",
+  "weCareData"      => "We care about your data. Read our :p",
 ];
